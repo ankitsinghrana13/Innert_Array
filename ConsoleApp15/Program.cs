920079c7bc2b5fd36cbe654605edc3050743cfc8
ankitsinghrana13 <ak9897432538@gmail.com>
2022-02-28 06:12:18 +0800

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Insert_array
+{
+
+    class program
+    {
+
+        static void Main(string[] arg)
+        {
+
+            int[] x = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("enter the array elelemnt");
+                x[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("your insert array elements");
+            foreach (int arr in x)
+            {
+                Console.WriteLine(arr);
+            }
+            Console.ReadLine();
+        }
+    }
+}
